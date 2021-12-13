6e86ad11fbe589b5270c8e6f65b118ef3cc89615
Joseph Goolic <joegoolic15@gmail.com>
2021-12-12 20:27:19 -0500

    test url change

--- config/index.js
+++ config/index.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://localhost:8000';
+//export const API_URL = 'http://localhost:8000';
 //export const API_URL = 'http://api.milassweetshop.com'
+export const API_URL = 'http://67.250.105.65:8000';
